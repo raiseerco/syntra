127985b6d2e96a6c0bd380fac088a814596e5cf8
Leo Sagan <raiseerco@gmail.com>
2024-11-29 03:25:40 -0300

    Pic storage WIP

--- src/app/components/ui/AvatarProfile.tsx
+++ src/app/components/ui/AvatarProfile.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
+import { setBlockie, shortAddress } from '../../../lib/utils';
 
+import Chip from './Chip';
 import Image from 'next/image';
-import { setBlockie } from '../../../lib/utils';
 
 interface AvaatarProfileProps {
   address: string;
@@ -16,6 +17,8 @@ const AvatarProfile: React.FC<AvaatarProfileProps> = ({ address }) => {
   const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
+    setBlockie(canvasRef, address);
+
     async function fetchUser(address: string) {
       try {
         const p = `/api/user?address=${address}`;
@@ -37,35 +40,30 @@ const AvatarProfile: React.FC<AvaatarProfileProps> = ({ address }) => {
     fetchUser(address).then((u: any) => {
       if (u) {
         setUser(u.data);
-      } else {
-        setBlockie(canvasRef, address);
       }
     });
   }, [address]);
 
   return (
-    <div className="py-1 px-2 rounded-full justify-center items-center gap-1 inline-flex">
-      <div className="justify-center items-center gap-1 flex">
-        {!user ? (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            className="rounded-full w-16 h-16 object-cover"
-            src={user?.profileImage}
-            alt="profile"
-          />
-        ) : (
-          <canvas
-            ref={canvasRef}
-            width={26}
-            height={26}
-            className="w-16 h-16 rounded-[14px] border border-stone-400"
-          />
-        )}
+    <div className="justify-center items-center gap-1 flex flex-col">
+      {user ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          className="rounded-full w-16 h-16 object-cover"
+          src={user?.profileImage}
+          alt="profile"
+        />
+      ) : (
+        <canvas
+          ref={canvasRef}
+          width={26}
+          height={26}
+          className="w-16 h-16 rounded-full border border-stone-400"
+        />
+      )}
 
-        <div className="text-center text-neutral-700 dark:text-neutral-300 text-sm">
-          {address}
-        </div>
-      </div>
+      <p className="text-xl">{user?.displayName}</p>
+      <p className="text-sm">{shortAddress(address)}</p>
     </div>
   );
 };
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,80 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import AvatarProfile from '../components/ui/AvatarProfile';
 import { Button } from '../components/ui/Button';
 import Chip from '../components/ui/Chip';
 import PlatformLayout from '../layouts/platformLayout';
 import { getDocument } from '../../lib/firestore';
 import { shortAddress } from '../../lib/utils';
-import { useEffect } from 'react';
 import { usePrivy } from '@privy-io/react-auth';
 
 export default function Profile() {
   const { login, logout, user, authenticated } = usePrivy();
 
+  type Profile = {
+    firstName?: string | null | undefined;
+    lastName?: string | null | undefined;
+    email?: string | null | undefined;
+    role?: string | null | undefined;
+    displayName?: string | null | undefined;
+    timezone?: string | null | undefined;
+    profileImage?: string | null | undefined;
+    coverImage?: string | null | undefined;
+    isAdmin?: boolean | null | undefined;
+  };
+
+  const [profile, setProfile] = useState<Profile | null>(null);
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const p = `/api/user?address=${user?.wallet?.address}`;
+        const response = await fetch(p, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) throw new Error('Failed to fetch document');
+        const data = await response.json();
+        console.log('data ', data);
+        setProfile(data.data);
+      } catch (error) {
+        console.error(`Error reading:`, error);
+      }
+    }
+
+    if (user?.wallet?.address) {
+      fetchProfile();
+    }
+  }, [user]);
+
+  const handleSave = () => {
+    console.log('profile ', profile);
+    // try {
+    //   const p = `/api/user?address=${user?.wallet?.address}`;
+    //   const response = await fetch(p, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(profile),
+    //   });
+
+    //   if (!response.ok) throw new Error('Failed to fetch document');
+    //   const data = await response.json();
+    //   console.log('data ', data);
+    // } catch (error) {
+    //   console.error(`Error reading:`, error);
+    // }
+  };
+
   return (
     <PlatformLayout>
-      <div className="mb-4 flex w-full flex-col gap-4 px-8">
+      <div className="mb-4 flex w-full flex-col gap-20 px-8">
         {/* header */}
         <div className=" bg-stone-100 dark:bg-stone-700 rounded-xl p-4 flex flex-col my-4 shadow-md">
           <div
@@ -23,100 +83,116 @@ export default function Profile() {
               backgroundSize: 'cover',
               backgroundPosition: 'center',
             }}
-            className="min-h-[25vh] rounded-xl"></div>
-          <div className="flex items-center justify-between">
-            <div className="flexd max-w-[50%] flex-col">
-              <div className="rounded-xl pl-2 text-xs font-mono pr-4 z-20 dark:text-stone-300">
-                {/* <Chip text={shortAddress(user?.wallet?.address)} /> */}
-                <AvatarProfile
-                  address={
-                    user?.wallet?.address ||
-                    'https://avatars.githubusercontent.com/u/56138448?s=52&v=4'
-                  }></AvatarProfile>
-              </div>
+            className="min-h-[15vh] rounded-xl">
+            {/* cover */}
+          </div>
 
-              <div className="mt-2 flex items-center">
-                <span className="mb-1 mr-1 text-lg font-bold">
-                  {user?.wallet?.address}
-                </span>
-              </div>
-              {/* <p className="mt-1 font-medium">{bio}</p> */}
+          <div className="relative flex items-center bg-purple-700 justify-between">
+            <div className="absolute -top-5 sz-20 dark:text-stone-300">
+              <AvatarProfile
+                address={
+                  user?.wallet?.address ||
+                  'https://avatars.githubusercontent.com/u/56138448?s=52&v=4'
+                }
+              />
             </div>
           </div>
         </div>
 
-        <div className=" bg-stone-100 rounded-xl p-8 dark:bg-stone-700">
-          <div className="rounded-xl w-full p-4 flex gap-2">
+        <div className="rounded-xl pt-4 gap-4 flex flex-col text-stone-600 dark:text-stone-400 ">
+          <div className="rounded-xl w-full flex gap-4">
             <div className="flex flex-col w-full gap-2">
-              First name
+              <span className="text-sm">First name</span>
               <input
                 className="py-2 px-3 w-full
-            placeholder:dark:text-stone-600 
+            placeholder:dark:text-stone-500 
             placeholder:text-stone-300
             bg-white dark:bg-stone-700
             outline-none rounded-sm"
                 placeholder="Enter your first name..."
                 type="text"
+                value={profile?.firstName ?? ''}
+                onChange={e =>
+                  setProfile({ ...profile, firstName: e.target.value })
+                }
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              Last name
+              <span className="text-sm">Last name</span>
               <input
                 className="py-2 px-3 w-full
-            placeholder:dark:text-stone-600 
+            placeholder:dark:text-stone-500 
             placeholder:text-stone-300
             bg-white dark:bg-stone-700
             outline-none rounded-sm"
                 placeholder="Enter your last name..."
                 type="text"
+                value={profile?.lastName ?? ''}
+                onChange={e =>
+                  setProfile({ ...profile, lastName: e.target.value })
+                }
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              Email
+              <span className="text-sm">Email</span>
               <input
                 className="py-2 px-3 w-full
-            placeholder:dark:text-stone-600 
+            placeholder:dark:text-stone-500 
             placeholder:text-stone-300
             bg-white dark:bg-stone-700
             outline-none rounded-sm"
                 placeholder="Enter your email..."
                 type="text"
+                value={profile?.email ?? ''}
+                onChange={e =>
+                  setProfile({ ...profile, email: e.target.value })
+                }
               />
             </div>
           </div>
-          <div className="rounded-xl w-full p-4 flex gap-2">
+          <div className="rounded-xl w-ful flex gap-4">
             <div className="flex flex-col w-full gap-2">
-              Role
+              <span className="text-sm">Role</span>
               <input
                 className="py-2 px-3 w-full
-            placeholder:dark:text-stone-600 
+            placeholder:dark:text-stone-500 
             placeholder:text-stone-300
             bg-white dark:bg-stone-700
             outline-none rounded-sm"
                 type="text"
+                value={profile?.role ?? ''}
+                onChange={e => setProfile({ ...profile, role: e.target.value })}
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              Display name
+              <span className="text-sm">Display name</span>
               <input
                 className="py-2 px-3 w-full
-            placeholder:dark:text-stone-600 
+            placeholder:dark:text-stone-500 
             placeholder:text-stone-300
             bg-white dark:bg-stone-700
             outline-none rounded-sm"
                 placeholder="Enter your display name..."
                 type="text"
+                value={profile?.displayName ?? ''}
+                onChange={e =>
+                  setProfile({ ...profile, displayName: e.target.value })
+                }
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              Timezone
+              <span className="text-sm">Timezone</span>
               <input
+                onChange={e =>
+                  setProfile({ ...profile, timezone: e.target.value })
+                }
                 className="py-2 px-3 w-full
-            placeholder:dark:text-stone-600 
+            placeholder:dark:text-stone-500 
             placeholder:text-stone-300
             bg-white dark:bg-stone-700
             outline-none rounded-sm"
                 type="text"
+                value={profile?.timezone ?? ''}
               />
             </div>
           </div>
@@ -124,10 +200,10 @@ export default function Profile() {
 
         <div className="text-right">
           <button
-            // onClick={handleSave}
-            disabled
+            onClick={handleSave}
             className="text-xs rounded-md px-3 py-2 mt-2
-                             dark:hover:bg-stone-700
+                             dark:hover:bg-stone-600
+                             dark:bg-stone-700
                              bg-stone-200
                              dark:text-stone-400 text-stone-900">
             Save changes
