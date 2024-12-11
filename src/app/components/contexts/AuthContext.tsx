2b3361a62ea3ffd079b2f817a909aee9e8d7b4b3
Leo Sagan <raiseerco@gmail.com>
2024-12-10 23:57:49 -0300

    Renames env vars

--- src/app/components/contexts/AuthContext.tsx
+++ src/app/components/contexts/AuthContext.tsx
@@ -11,17 +11,8 @@ import { User, usePrivy } from '@privy-io/react-auth';
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth, signInWithCustomToken, signOut } from 'firebase/auth';
 
+import { firebaseConfig } from '../../../lib/firebaseConfig';
 import { useRouter } from 'next/navigation';
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-};
 
 let firebaseApp;
 
--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,6 +1,5 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 
-// import { cert } from 'firebase-admin/app';
 import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 
@@ -9,17 +8,18 @@ const serviceAccount = JSON.parse(
 );
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-
-  messagingSenderId: '37949963488',
-  appId: '1:37949963488:web:90296ecb9a4b25d2f0a485',
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-
-  // credential: cert(serviceAccount),
+  apiKey: process.env.FIREBASE_API_KEY as string,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET_CLIENT as string,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  clientEmail: process.env.FIREBASE_EMAIL as string,
+  privateKey: process.env.FIREBASE_ADMIN_PK as string,
 };
+
+const params = {};
 
 let app;
 if (getApps().length === 0) {
