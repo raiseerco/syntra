4f1cc5497e36eab4ce01c941c88634b3bc3e4937
Leo Sagan <raiseerco@gmail.com>
2025-01-17 05:18:52 -0300

    Fix vars

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -4,24 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // apiKey: process.env.FIREBASE_API_KEY as string,
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
-  // databaseURL: process.env.FIREBASE_DB_URL as string,
-  // projectId: process.env.FIREBASE_PROJECT_ID as string,
-  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET_CLIEN as string,
-  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
-  // appId: process.env.FIREBASE_APP_ID as string,
-
-  // clientEmail: process.env.FIREBASE_EMAIL as string,
-  // privateKey: process.env.FIREBASE_ADMIN_PK as string,
-
-  apiKey: 'AIzaSyA5a2g9h6JZkiIHxpwzJ00WNsZa69Rmfk4',
-  authDomain: 'demosphere-web3.firebaseapp.com',
-  databaseURL: 'https://demosphere-web3-default-rtdb.firebaseio.com',
-  projectId: 'demosphere-web3',
-  storageBucket: 'demosphere-web3.firebasestorage.app',
-  messagingSenderId: '37949963488',
-  appId: '1:37949963488:web:90296ecb9a4b25d2f0a485',
+  apiKey: process.env.FIREBASE_API_KEY as string,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+  databaseURL: process.env.FIREBASE_DB_URL as string,
+  projectId: process.env.FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET_CLIEN as string,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.FIREBASE_APP_ID as string,
 
   clientEmail: process.env.FIREBASE_EMAIL as string,
   privateKey: process.env.FIREBASE_ADMIN_PK as string,
